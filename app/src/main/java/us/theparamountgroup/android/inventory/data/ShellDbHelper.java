dd0f9c9a20243d9a22255ddfed4c0b586d76aef3
Bob Ingram <bob@theparamountgroup.us>
2017-03-11 20:44:42 -0500

    Working version table created with photo collumn added

--- app/src/main/java/us/theparamountgroup/android/inventory/data/ShellDbHelper.java
+++ app/src/main/java/us/theparamountgroup/android/inventory/data/ShellDbHelper.java
@@ -56,8 +56,8 @@ public class ShellDbHelper extends SQLiteOpenHelper {
                 + PetEntry.COLUMN_SHELL_NAME + " TEXT NOT NULL, "
                 + PetEntry.COLUMN_SHELL_COLOR + " TEXT, "
                 + PetEntry.COLUMN_SHELL_HOLE + " INTEGER NOT NULL, "
-                + PetEntry.COLUMN_SHELL_TYPE + " INTEGER NOT NULL);,";
-          //      + PetEntry.COLUMN_SHELL_PHOTO + "TEXT);";
+                + PetEntry.COLUMN_SHELL_TYPE + " INTEGER NOT NULL,"
+                + PetEntry.COLUMN_SHELL_PHOTO + "TEXT);";
 
         // Execute the SQL statement
         db.execSQL(SQL_CREATE_PETS_TABLE);
